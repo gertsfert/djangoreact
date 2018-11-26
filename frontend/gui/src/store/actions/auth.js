23e67fcdf82dbd7fee004fe6e8709846f669e501
gertsfert <gertsfert@gmail.com>
2018-11-26 13:05:28 +1100

    formatting changes - starting to implement authentication between backend and frontend

---
+++ frontend/gui/src/store/actions/auth.js
@@ -0,0 +1,59 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = token => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    token: token
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
+
+export const logout = () => {
+  localStorage.removeItem("user");
+  localStorage.removeItem("expirationDate");
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
+export const authLogin = (username, password) => {
+  return dispatch => {
+    dispatch(authStart());
+    axios
+      .post("http://127.0.0.1:8000/rest-auth/login/", {
+        username: username,
+        password: password
+      })
+      .then(res => {
+        const token = res.data.key;
+        // expiration time is set per millisecond
+        // seems kinda strange that this will be stored on the client side?
+        const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+        localStorage.setItem("token", token);
+        localStorate.setItem("expirationDate", expirationDate);
+        dispatch(authSuccess(token));
+        dispatch(checkAuthTimeout(3600));
+      });
+  };
+};
